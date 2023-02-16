a1b27cd28c675ce98a21984866b9d3b69faf9aa9
PavelYrevich <dj.2009@mail.ru>
2023-02-16 16:11:21 +0300

    Домашее задание

---
+++ seminar1/HomeWork10.02.23.cs
@@ -0,0 +1,83 @@
+// ДОМАШНЕЕ ЗАДАНИЕ №1
+
+
+// Console.WriteLine("Enter first number:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter second number:");
+
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num > num2)
+// {
+// Console.WriteLine("The first number " + num + " is greater " + num2 );
+
+// }
+
+// else
+
+// {
+
+// Console.WriteLine("The second number " + num2 + " is greater " + num );
+
+// }
+
+// ДОМАШНЕЕ ЗАДАНИЕ №2 
+
+// Console.WriteLine("Enter tree numbers:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int num3 = Convert.ToInt32(Console.ReadLine());
+            
+// int max = num1;
+
+// if (num2 > max)
+// {
+// max = num2;
+// }
+
+// if (num3 > max)
+// {
+// max = num3;
+// }
+
+// Console.WriteLine("The largest of the entered numbers is the number " + max);
+
+// ДОМАШНЕЕ ЗАДАНИЕ №3
+
+
+
+// Console.WriteLine("Enter number ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 1)
+// {
+// Console.WriteLine("The number " + num + " is not even");
+//  }
+// else
+// {
+// Console.WriteLine("The number " + num + " is even");
+// }
+
+// ДОМАШНЕЕ ЗАДАНИЕ №4
+
+// int i = 1;
+// bool not = true;
+
+// Console.WriteLine("Enter number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter numbers from 1 to" + num);
+// while (i <= num)
+// {
+// if (i % 2 != 1)
+// {
+// Console.Write(i + ", ");
+// not = false;
+// }
+// i++;
+// }
+
+// if (not)
+// {
+// Console.WriteLine("There are no even numbers");
+// }
